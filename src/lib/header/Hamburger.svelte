60431c68f08b6f1052c5b96d900d4ab535a8d14e
lums <jeremy@lums.io>
2022-09-02 11:41:28 +0200

    svg button toggle

--- src/lib/header/Hamburger.svelte
+++ src/lib/header/Hamburger.svelte
@@ -4,19 +4,22 @@
 </script>
 
 <div class="burgermenu">
-	<!-- <button /> -->
-	<!-- <svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect x="0.5" y="0.5" width="28.9556" height="28.9556" fill="#2C2C2C" stroke="#2C2C2C" />
-		<path d="M8 6L8 24" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-		<path d="M15 6L15 21" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-		<path d="M22 6L22 23" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-	</svg> -->
-	<svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect x="0.5" y="0.5" width="28.9556" height="28.9556" fill="#2C2C2C" stroke="#2C2C2C" />
-		<path d="M8 27V30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-		<path d="M15 27V30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-		<path d="M22 27L22 30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
-	</svg>
+	<button on:click />
+	{#if menuState}
+		<svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="0.5" y="0.5" width="28.9556" height="28.9556" fill="#2C2C2C" stroke="#2C2C2C" />
+			<path d="M8 6L8 24" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+			<path d="M15 6L15 21" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+			<path d="M22 6L22 23" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+		</svg>
+	{:else}
+		<svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="0.5" y="0.5" width="28.9556" height="28.9556" fill="#2C2C2C" stroke="#2C2C2C" />
+			<path d="M8 27V30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+			<path d="M15 27V30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+			<path d="M22 27L22 30" stroke="#EAEAEA" stroke-width="3" stroke-linecap="round" />
+		</svg>
+	{/if}
 </div>
 
 <style>
@@ -27,9 +30,15 @@
 		border: white solid 1px;
 	}
 	button {
+		position: absolute;
+		top: 0;
 		width: 100%;
 		height: 100%;
 		border: none;
 		background-color: #444;
+		opacity: 0;
+	}
+	button:hover {
+		cursor: crosshair;
 	}
 </style>
