9f392aeb7de877c658f7ec6afdd8827307396765
lums <jeremy@lums.io>
2022-09-07 11:46:22 +0200

    Add test images

--- src/routes/galerie/+page.svelte
+++ src/routes/galerie/+page.svelte
@@ -1,1 +1,41 @@
-<p>hier bilder</p>
+<div class="sec">
+	<h2>namaste liebe freunde</h2>
+	<picture><img src="https://source.unsplash.com/random/200×100/?fruit" alt="" srcset="" /></picture
+	>
+	<picture><img src="https://source.unsplash.com/random/200×100/?dog" alt="" srcset="" /></picture>
+	<picture><img src="https://source.unsplash.com/random/200×100/?cat" alt="" srcset="" /></picture>
+	<picture
+		><img src="https://source.unsplash.com/random/200×100/?animal" alt="" srcset="" /></picture
+	>
+	<picture><img src="https://source.unsplash.com/random/200×100/?fruit" alt="" srcset="" /></picture
+	>
+	<picture><img src="https://source.unsplash.com/random/200×100/?bird" alt="" srcset="" /></picture>
+	<picture
+		><img src="https://source.unsplash.com/random/200×100/?sealion" alt="" srcset="" /></picture
+	>
+	<picture><img src="https://source.unsplash.com/random/200×100/?whale" alt="" srcset="" /></picture
+	>
+	<picture
+		><img src="https://source.unsplash.com/random/200×100/?manatee" alt="" srcset="" /></picture
+	>
+	<picture><img src="https://source.unsplash.com/random/200×100/?birb" alt="" srcset="" /></picture>
+	<picture><img src="https://source.unsplash.com/random/200×100/?meme" alt="" srcset="" /></picture>
+	<picture><img src="https://source.unsplash.com/random/200×100/?test" alt="" srcset="" /></picture>
+</div>
+
+<style>
+	h2 {
+		flex-basis: 1;
+		font-size: 2rem;
+	}
+	.sec {
+		display: flex;
+		flex-wrap: wrap;
+		margin-top: 5rem;
+	}
+	img {
+		width: 100px;
+		height: auto;
+		padding: 0.1rem;
+	}
+</style>
